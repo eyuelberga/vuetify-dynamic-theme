9b755eaf23e359a570af2f7b81ff0e8367e0d235
eyuelberga <eyuel.berga@yahoo.com>
2020-08-25 17:45:49 +0300

    modify vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  publicPath: process.env.NODE_ENV === "production" ? "/dynamic-theme/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/vuetify-dynamic-theme/" : "/"
 };
